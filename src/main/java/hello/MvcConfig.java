ddead6c2c4571508f53180cc0fd14bea27bdcaca
Santi <santii.barboza@gmail.com>
2018-10-25 19:00:56 -0300

    first commit

--- src/main/java/hello/MvcConfig.java
+++ src/main/java/hello/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/ide").setViewName("index");
+        registry.addViewController("/ide").setViewName("ide");
     }
 
 }
