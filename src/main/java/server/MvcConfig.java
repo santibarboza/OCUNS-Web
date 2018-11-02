9abbf56cf6f7e5155d928ed2a85af9aacd3b9bb3
Santi <santii.barboza@gmail.com>
2018-11-02 07:43:22 -0300

    xs

--- src/main/java/server/MvcConfig.java
+++ src/main/java/server/MvcConfig.java
@@ -11,6 +11,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/ide").setViewName("index");
+        registry.addViewController("/ide/new").setViewName("index");
         registry.addViewController("/ide/file").setViewName("file");
         registry.addViewController("/help").setViewName("help");
         registry.addViewController("/about").setViewName("about");
